7b4281ddd3e9f5f50c66a4d5efe016744ef996a7
subhadeep-webart <subhadeep.das@webart.technology>
2025-07-30 21:33:44 +0530

    done

--- src/components/profile/ProfileInfo/index.jsx
+++ src/components/profile/ProfileInfo/index.jsx
@@ -1,27 +1,32 @@
 import Image from "next/image";
 import styles from "../profile.module.scss";
 import ProfileNavigation from "./ProfileNavigation";
-const ProfileInfo = ({ profileNavMenu, name, userName,profile_img }) => {
-    return (
-        <div className={styles.profile_info_wrapper}>
-            <div className={styles.profile_image}>
-                <Image src={profile_img ? profile_img : "/assets/images/user-avatar.jpg"} fill className="object-cover" />
-            </div>
-            <div className={styles.profile_name}>
-                <div>
-                    <p className={styles.profile_name_text}>{name}</p>
-                    <p className="text-[#9D9D9D]">@{userName}</p>
-                </div>
-                <div className={styles.profile_navigation}>
-                    {
-                        profileNavMenu.map((profileNav, index) => (
-                            <ProfileNavigation key={`profile_menu_${index + 1}`} profileNav={profileNav} />
-                        ))
-                    }
-                </div>
-            </div>
+const ProfileInfo = ({ profileNavMenu, name, userName, profile_img }) => {
+  return (
+    <div className={styles.profile_info_wrapper}>
+      <div className={styles.profile_image}>
+        <Image
+          src={profile_img ? profile_img : "https://images.unsplash.com/broken"}
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className={styles.profile_name}>
+        <div>
+          <p className={styles.profile_name_text}>{name}</p>
+          <p className="text-[#9D9D9D]">@{userName}</p>
         </div>
-    );
-}
+        <div className={styles.profile_navigation}>
+          {profileNavMenu.map((profileNav, index) => (
+            <ProfileNavigation
+              key={`profile_menu_${index + 1}`}
+              profileNav={profileNav}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProfileInfo;
--- src/components/ui/ProfileDropdown/index.jsx
+++ src/components/ui/ProfileDropdown/index.jsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 import { useLogoutHandler } from "@/hooks/useLogoutHandler";
 import { PROFILE_DROPDOWN_MENU_ITEM } from "@/utils/constant";
 import {
-    Dropdown,
-    DropdownTrigger,
-    DropdownMenu,
-    DropdownItem
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
 } from "@heroui/react";
 
 import { Avatar } from "@heroui/react";
@@ -13,38 +13,48 @@ import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 const ProfileDropdown = ({ profile_img }) => {
-    const handleLogout = useLogoutHandler();
+  const handleLogout = useLogoutHandler();
 
-    const handleUserLogout = () => {
-        console.log("Logout call");
-        handleLogout();
-    }
-    return (
-        <Dropdown placement="bottom-end">
-            <DropdownTrigger>
-                <Avatar
-                    isBordered
-                    as="button"
-                    className="transition-transform object-cover !bg-gray-900"
-                    src={profile_img ? profile_img : "https://images.unsplash.com/broken"}
-                />
-            </DropdownTrigger>
-            <DropdownMenu aria-label="Profile Actions" className="bg-black !px-4 !py-2 !rounded-lg">
-                {
-                    PROFILE_DROPDOWN_MENU_ITEM.map((dropdownMenu) => (
-                        <DropdownItem startContent={dropdownMenu.startContent} className="text-white !py-2 !px-2">
-                            <Link href={dropdownMenu.link} className="w-full h-full block">
-                                {dropdownMenu.label}
-                            </Link>
-                        </DropdownItem>
-                    ))
-                }
-                <DropdownItem key="logout" color="danger" startContent={<LogOut size={20} />} className="text-white !py-2 !px-2" onClick={handleUserLogout}>
-                    Log Out
-                </DropdownItem>
-            </DropdownMenu>
-        </Dropdown>
-    )
-}
+  const handleUserLogout = () => {
+    console.log("Logout call");
+    handleLogout();
+  };
+  return (
+    <Dropdown placement="bottom-end">
+      <DropdownTrigger>
+        <Avatar
+          isBordered
+          as="button"
+          className="transition-transform object-cover !bg-gray-900"
+          src={profile_img ? profile_img : "https://images.unsplash.com/broken"}
+        />
+      </DropdownTrigger>
+      <DropdownMenu
+        aria-label="Profile Actions"
+        className="bg-black !px-4 !py-2 !rounded-lg"
+      >
+        {PROFILE_DROPDOWN_MENU_ITEM.map((dropdownMenu) => (
+          <DropdownItem
+            startContent={dropdownMenu.startContent}
+            className="text-white !py-2 !px-2"
+          >
+            <Link href={dropdownMenu.link} className="w-full h-full block">
+              {dropdownMenu.label}
+            </Link>
+          </DropdownItem>
+        ))}
+        <DropdownItem
+          key="logout"
+          color="danger"
+          startContent={<LogOut size={20} />}
+          className="text-white !py-2 !px-2"
+          onClick={handleUserLogout}
+        >
+          Log Out
+        </DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
 
-export default ProfileDropdown
+export default ProfileDropdown;
